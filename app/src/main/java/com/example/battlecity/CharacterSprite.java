c75143e2a43a604da8beb6f85ebb921232009be5
Matias <matiaspippig@gmail.com>
2022-04-04 20:11:02 -0300

    Flappy bird

--- app/src/main/java/com/example/battlecity/CharacterSprite.java
+++ app/src/main/java/com/example/battlecity/CharacterSprite.java
@@ -6,9 +6,9 @@ import android.graphics.Canvas;
 
 public class CharacterSprite {
     private Bitmap image;
-    private int x,y;
+    public int x,y;
     private int xVelocity = 10;
-    private int yVelocity = 5;
+    public int yVelocity = 5;
     private int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
     private int screenHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
     public CharacterSprite(Bitmap bmp) {
@@ -22,14 +22,7 @@ public class CharacterSprite {
     }
 
     public void update() {
-        x += xVelocity;
         y += yVelocity;
-        if ((x > screenWidth - image.getWidth()) || (x < 0)) {
-            xVelocity = xVelocity*-1;
-        }
-        if ((y > screenHeight - image.getHeight()) || (y < 0)) {
-            yVelocity = yVelocity*-1;
-        }
 
     }
 }
